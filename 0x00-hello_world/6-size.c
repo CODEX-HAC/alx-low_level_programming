ce753a5aa2459d1b7c6919eae2e7e9720edd3b7a
chivel <marvellousmichael00@gmmail.com>
2022-12-08 09:18:42 -0800

    commit -m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - print my data in bit (marvellous will appriate)
- * code by marvellous 
- * Return: 0 (as it run successfull)
+ *main - Entry point
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char m;
-	int a;
-	long int r;
-	long long int v;
-	float e;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(m));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(r));
-	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(v));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
-
-	return (0);
-	}
+	 printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	 printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	 printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	 return (0);
+}
