83f73eb3fd9520730db15a5b491a1cb4ab33287a
chivel <marvellousmichael00@gmmail.com>
2022-12-09 14:28:12 -0800

    commit task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,32 +5,16 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
-*/
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	int p, q;
+	int i;
 
-	for (p = 0; p <= 98; p++)
-	{
-		for (q = p + 1; q <= 99; q++)
-		{
-			putchar((p / 10) + '0');
-			putchar((p % 10) + '0');
-			putchar(' ');
-			putchar((q / 10) + '0');
-			putchar((q % 10) + '0');
-
-			if (p == 98 && q == 99)
-				continue;
-
-			putchar(',');
-			putchar(' ');
-		}
-	}
-
-	putchar('\n');
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
 
 	return (0);
 }
